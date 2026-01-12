3d3fbd19c1c9cfe20d0739a076ea96dbe83b03a8
MAT06mat <mat06mat22@gmail.com>
2026-01-12 15:05:10 +0100

    Update API_URL to point to the correct server

--- src/data/api.ts
+++ src/data/api.ts
@@ -1,2 +1,2 @@
-export const API_URL = "http://mat06mat.github.io/bricedeliste/api/";
+export const API_URL = "http://chantemuse.fr/bricedeliste/api/";
 export const REGISTER_URL = API_URL + "register.php";
