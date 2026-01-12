64fb84a3d3342b1d47c89bb81aa9e95d7b35cdad
MAT06mat <mat06mat22@gmail.com>
2026-01-12 15:05:30 +0100

    Fix API_URL typo in endpoint

--- src/data/api.ts
+++ src/data/api.ts
@@ -1,2 +1,2 @@
-export const API_URL = "http://chantemuse.fr/bricedeliste/api/";
+export const API_URL = "http://chantemuse.fr/brice-de-liste/api/";
 export const REGISTER_URL = API_URL + "register.php";
