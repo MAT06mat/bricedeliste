e369861a4a91bbaf2deeb0b170f3205e5e4c7746
MAT06mat <mat06mat22@gmail.com>
2026-02-13 18:06:52 +0100

    Update api url

--- src/data/api.ts
+++ src/data/api.ts
@@ -1,3 +1,4 @@
 export const API_URL =
-    import.meta.env.VITE_API_URL || "https://chantemuse.fr/api/brice-de-liste/";
+    import.meta.env.VITE_API_URL ||
+    "https://api.bricedeliste.asso-insa-lyon.fr/";
 export const REGISTER_URL = API_URL + "register.php";
