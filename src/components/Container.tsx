93066a704ef666aeb05dd603a2d39ba80716a5b0
MAT06mat <mat06mat22@gmail.com>
2026-02-28 20:28:26 +0100

    Update Container component styles to adjust bottom margin

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -5,7 +5,7 @@ export default function Container({
 }: { children: React.ReactNode } & React.HTMLAttributes<HTMLDivElement>) {
     return (
         <div
-            className={`max-w-5xl md:px-4 mx-auto text-center space-y-12 animate-in fade-in zoom-in duration-700 ${className}`}
+            className={`max-w-5xl md:px-4 mx-auto text-center space-y-12 animate-in fade-in zoom-in duration-700 mb-0 md:mb-8 ${className}`}
             {...props}
         >
             {children}
