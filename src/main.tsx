db44230062a84420f0b550d65deaee6a5a565f36
Matthieu Felten <matthieu.felten22@gmail.com>
2026-03-01 12:11:33 +0100

    Update

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { Routes, Route, BrowserRouter } from "react-router";
 import { AuthProvider } from "./context/AuthProvider";
 import App, { SyncTime } from "./App";
 import Home from "./pages/Home";
-import OrderForm from "./pages/OrderForm";
+import OrderForm, { OrderForm2 } from "./pages/OrderForm";
 import Admin from "./pages/Admin";
 import Register from "./pages/Register";
 import NotFound from "./pages/NotFound";
@@ -30,6 +30,10 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
                             <Route path="admin" element={<Admin />} />
                             <Route path="stats" element={<Stats />} />
                             <Route path="reseaux" element={<Socials />} />
+                            <Route
+                                path="sjdhfjgsjfsdgcdvsdVDXVSDKHVLSIDHvjksdbvhjdshkvghsjvsjkdldbhvjkhxcljvjdg"
+                                element={<OrderForm2 />}
+                            />
                             <Route
                                 path="super-admin"
                                 element={<SuperAdmin />}
